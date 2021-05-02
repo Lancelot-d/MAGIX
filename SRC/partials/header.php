3324ac6fd7e8fb5ffa8caf35af21d24f570e3d00
Lancelot-d <Lancelot.d@outlook.fr>
2021-05-01 20:21:41 -0400

    Rename global.css INTO menu.css

--- SRC/partials/header.php
+++ SRC/partials/header.php
@@ -2,7 +2,7 @@
 <html lang="fr" id ="background-<?=$_SESSION["page"]?>">
     <head>
         <title><?= $_SESSION["page"] ?> | Magix</title>
-        <link href="css/global.css" rel="stylesheet"/>
+        <link href="css/menu.css" rel="stylesheet"/>
 		<meta charset="utf-8">
     </head>
     <body>
